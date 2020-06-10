f308bea0bc556d40032127923840ab0092d5093f
Stheven Cabral <stheven.cabral@gmail.com>
2020-06-09 18:02:34 -0700

    06092020  1802

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('basic-auth');
 const models = require('../models');
 const { User } = models;
+
+// Authentication middleware
+const authenticateUser = async (req, res, next) => {
+  const users = await User.findAll();
+  const credentials = auth(req);
+  if (credentials) {
+    const user = users.find(u => u.emailAddress === credentials.name);
+    // STOPPED HERE
+  }
+  // next();
+}
 
 function asyncHandler (cb) {
   return async (req,res,next)=> {
@@ -15,9 +27,10 @@ function asyncHandler (cb) {
   }
 }
 
-router.get('/users', asyncHandler( async (req, res) => {
-  const users = await User.findAll();
-  res.send(users);
+router.get('/users', authenticateUser, asyncHandler( async (req, res) => {
+  console.log('Hello');
+  // const users = await User.findAll();
+  // res.send(users);
 }));
 
 
